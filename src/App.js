07e27e3ac27b67405a2c48e1f82e55433d1f785c
Prashant Gupta <prashantgupta2507@gmail.com>
2022-02-02 12:28:19 +0530

    Main Page with Login and Register Design Completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from 'react';
 import './App.css';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import NavbarMain from './Components/NavbarMain';
 
 function App() {
   return (
-    <div>
+    <>
+    <div className="App" style={{height:'90vh'}}>
+      <NavbarMain/>
     </div>
+    <About/>
+    <Contact/>
+    </>
   );
 }
 
